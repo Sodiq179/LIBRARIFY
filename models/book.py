4850bff48b7290e5dc30062bf0144e1bf44421ed
Sodiq179 <babawalesodiq996@gmail.com>
2022-12-02 21:50:00 +0000

    [Storage]:DataBase

--- models/book.py
+++ models/book.py
@@ -2,15 +2,32 @@
 """
 User creation class
 """
-from models.librarify_base import LibrarifyBase
+from models.librarify_base import LibrarifyBase, Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+import models
+import shlex
+from os import getenv
 
 
-class Book(LibrarifyBase):
+class Book(LibrarifyBase, Base):
     """Defines attributes for user creation"""
-    author = ""
-    title = ""
-    category = ""
-    publication_year = 0
-    image_url = ""
-    book_api  = ""
-    publisher = ""
+    __tablename__ = "books"
+    if getenv("LIBRARIFY_TYPE_STORAGE") == "db":
+        author = Column(String(128), nullable=True)
+        title = Column(String(128), nullable=True)
+        category = Column(String(128))
+        publication_year = Column(Integer, nullable=True)
+        image_url = Column(String(128), nullable=True)
+        book_api  = Column(String(128), nullable=True)
+        publisher = Column(String(128), nullable=True)
+
+    else:
+        author=""
+        title=""
+        category=""
+        publication_year=0
+        image_url=""
+        book_api=""
+        publisher=""
--- models/user.py
+++ models/user.py
@@ -2,15 +2,24 @@
 """
 User creation class
 """
-from models.librarify_base import LibrarifyBase
+from os import getenv
+from models.librarify_base import LibrarifyBase, Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
-
-class User(LibrarifyBase):
+class User(LibrarifyBase, Base):
     """Defines attributes for user creation"""
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
-    user_name = ""
-    country = ""
-    mail_address = ""
+    __tablename__ = "users"
+    if getenv("LIBRARIFY_TYPE_STORAGE") == "db":
+        email = Column(String(128), nullable=True)
+        password =  Column(String(128), nullable=True)
+        first_name = Column(String(128), nullable=True)
+        last_name =  Column(String(128), nullable=True)
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+
